72dc0210fb0bd5be36c39e7efcd2dd0d25cdb26d
kirandash <thekirankumardash@gmail.com>
2017-08-02 12:19:56 +0530

    Add navbar component to app

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ cd gallery
 ng serve
 
 npm install ng2-bootstrap bootstrap --save (Deprecated npm install ngx-bootstrap bootstrap --save)
+
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+Add AlertModule to imports
+
+Add "../node_modules/bootstrap/dist/css/bootstrap.min.css" to styles in angular-cli.json
+
+Restart with ng server
+
+cd src/app
+
+mkdir navbar
+
+touch navbar.component.html navbar.component.css navbar.component.ts
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
